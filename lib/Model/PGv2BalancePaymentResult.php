361475ae1995d8406d59fa2f49c89f8d733ba0d0
Tomáš Kudělka <thorewi@gmail.com>
2025-04-01 10:36:48 +0200

    Update PGv2BalancePaymentResult.php

--- lib/Model/PGv2BalancePaymentResult.php
+++ lib/Model/PGv2BalancePaymentResult.php
@@ -193,7 +193,7 @@ class PGv2BalancePaymentResult implements ModelInterface, ArrayAccess
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->container['eshop_order_guid'] = isset($data['eshop_order_guid']) ? $data['eshop_order_guid'] : null;
         $this->container['price'] = isset($data['price']) ? $data['price'] : null;
