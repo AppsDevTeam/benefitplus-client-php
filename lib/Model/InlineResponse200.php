e09b1eed34b36c640d5678b725c85692b57edcbe
Tomáš Kudělka <thorewi@gmail.com>
2025-04-01 10:35:57 +0200

    Update InlineResponse200.php

--- lib/Model/InlineResponse200.php
+++ lib/Model/InlineResponse200.php
@@ -167,7 +167,7 @@ class InlineResponse200 implements ModelInterface, ArrayAccess
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
     }
 
